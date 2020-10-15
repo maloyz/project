221db4fc00ffb09a9426ca40a89b4a0d09e13e9c
Vlad <vladpanasik@mail.ru>
2020-10-15 21:55:20 +0300

    films

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-var age = prompt('Сколько вам лет? ');
-if (age >= 18) {
-    alert('Вам можно пить алкоголь)');
-}
-else {
-    alert('Ок.');
-}
-if (age >= 14, age < 18) {
-    alert('Вы подросток!');
-}
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
